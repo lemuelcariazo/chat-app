3e48bf0e3acba05200fbb7d5f959664cdf6eed38
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2022-12-23 22:34:44 +0800

    tailwind installed and preparing Server Side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log(count);
   return (
-    <div className="App">
+    <div className="App dark:bg-slate-900 dark:text-slate-50 bg-slate-200 text-slate-900">
       <>
-        <h1>Hello, World</h1>
-        <button onClick={(e) => setCount(count + 1)}></button>
+        <h1 className="p-2 border border-solid border-slate-900">
+          Hello, World
+        </h1>
+        <Button colorScheme="teal" variant="outline">
+          Click Me
+        </Button>
       </>
     </div>
   );
