a87147fe48332bec942857a5ec2e27b5054482af
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2022-12-23 23:28:18 +0800

    The Server is Up and Running Successfully

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const app = express()
-const cors = require("cors")
-require("dotenv").config()
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors)
-app.use(express.json)
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT | 5000
+app.get("/read", async (req, res) => {
+  res.send("Server is up and running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`The server is running at http://localhost:${PORT}`);
+});
