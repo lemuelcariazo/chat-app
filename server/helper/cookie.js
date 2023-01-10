7b796f89e513f0130ce282cee15407cd050b10bf
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2023-01-10 16:13:52 +0800

    fixing error cookies not saving

--- server/helper/cookie.js
+++ server/helper/cookie.js
@@ -2,7 +2,7 @@ const saveCookie = async (token, res) => {
   await res.cookie("jwt", token, {
     httpOnly: true,
     // secure: true,
-    // sameSite: "none",
+    sameSite: "none",
     // path: "/",
     expires: new Date(Date.now() + 3600000),
   });
