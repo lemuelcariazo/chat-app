da9281e931eb04e6578b673c282251a72a37df6a
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2023-01-10 16:22:54 +0800

    Fixing cookie error not saving

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dns from "dns"
-
+import dns from "dns";
 
 // https://vitejs.dev/config/
-dns.setDefaultResultOrder('verbatim')
+// dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   plugins: [react()],
