7e9ba87f27bd8c39ec108df193532e9c6407737b
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2023-01-11 18:54:35 +0800

    make the base url and server cors origin to be the same

--- client/src/hooks/useConfig.tsx
+++ client/src/hooks/useConfig.tsx
@@ -7,7 +7,7 @@ function useConfig() {
     // BASE_URL: "http://localhost:8080/api/",
     // BASE_URL: "https://chat-app-server-tan.vercel.app/api/",
     // BASE_URL: "https://chat-app-server-affe.onrender.com/api/",
-    BASE_URL: "https://chat-app-ruddy-one.vercel.app/api/",
+    BASE_URL: "https://chat-app-server-one.vercel.app/api/",
   };
 
   return { config };
