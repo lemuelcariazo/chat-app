8dca315e79cccd2551ee7717b8f94f02f1b33d0a
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2023-01-11 19:25:08 +0800

    making all my cookie option become false

--- server/helper/cookie.js
+++ server/helper/cookie.js
@@ -1,7 +1,7 @@
 const saveCookie = async (token, res) => {
   await res.cookie("jwt", token, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "none",
     path: "/",
     expires: new Date(Date.now() + 3600000),
