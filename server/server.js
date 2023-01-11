d35423cdc427ebd670496b4b37f72703918184db
xxductusxx <91464380+xxductusxx@users.noreply.github.com>
2023-01-11 18:12:48 +0800

    test hosted client to local server

--- server/server.js
+++ server/server.js
@@ -14,11 +14,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://chat-app-ruddy-one.vercel.app",
     credentials: true,
     allowedHeaders: ["Content-Type"],
-    // "http://localhost:5173",
-    // "https://chat-app-ruddy-one.vercel.app" ,
   })
 );
 
